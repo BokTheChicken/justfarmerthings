3c563f100fbec4b75e142326eb1c248b7ab5105a
Kerwin Sibunga <kerwinsibunga@gmail.com>
2022-12-09 17:58:43 +0800

    Responsive Change Theme button

--- src/header.js
+++ src/header.js
@@ -16,27 +16,25 @@ export default function Header() {
 
   useEffect(() => {
     if (theme === "dark") {
-      document.documentElement.classList.add("dark");
-    } else {
       document.documentElement.classList.remove("dark");
+    } else {
+      document.documentElement.classList.add("dark");
     }
   }, [theme]);
-
-  console.log("pressed");
 
   const handleThemeSwitch = () => {
     setTheme(theme === "dark" ? "light" : "dark");
   };
 
     return (
-      <div className="bg-emerald-500 mb-3 ">
+      <div className="bg-emerald-500 mb-3 w-auto h-auto relative ">
         <a
-        className="mx-8 text-2xl font-mono font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap text-white"
+        className=" mx-8 lg:text-2xl font-mono font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap text-white sm:text- "
         href="#placelinkhere">
         JustFarmerThings
         </a>
-        <button className="bg-yellow-300 absolute right-10 top-3 toggle-dark hover:bg-gray-600 rounded-xl toggle-dark" onClick={handleThemeSwitch}>
-          <p className="px-1 py-1 text-slate-600  hover:text-gray-100">
+        <button className="bg-gray-600 px-2 absolute right-5 top-1.5  dark:bg-yellow-300 rounded-xl hover:animate-pulse" onClick={handleThemeSwitch}>
+          <p className="px-1 py-1 text-gray-100 text-sm lg:text-lg  dark:text-slate-600">
           Change Theme</p>
         </button>
       </div>
